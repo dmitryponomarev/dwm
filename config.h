0e016660af6dbc4f43ff133b67bd28f9f4fe1978
dmitrysponomarev <dmitry.s.ponomarev@gmail.com>
2023-01-03 13:48:44 +0100

    test

--- config.h
+++ config.h
@@ -50,9 +50,9 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	*/
 	/* class    instance      title       	 tags mask    isfloating   isterminal  noswallow  monitor */
-	{ "Gimp",     NULL,       NULL,       	    1 << 8,       0,           0,         0,        -1 },
+	//{ "Gimp",     NULL,       NULL,       	    1 << 8,       0,           0,         0,        -1 },
 	//{ TERMCLASS,  NULL,       NULL,       	    0,            0,           1,         0,        -1 },
-	{ NULL,       NULL,       "Event Tester",   0,            0,           0,         1,        -1 },
+	//{ NULL,       NULL,       "Event Tester",   0,            0,           0,         1,        -1 },
 	//{ TERMCLASS,      "bg",        NULL,       	    1 << 7,       0,           1,         0,        -1 },
 	//{ TERMCLASS,      "spterm",    NULL,       	    SPTAG(0),     1,           1,         0,        -1 },
 	//{ TERMCLASS,      "spcalc",    NULL,       	    SPTAG(1),     1,           1,         0,        -1 },
