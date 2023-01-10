07a92b7bce71400be11c5210ea9a93899401623a
dmitrysponomarev <dmitry.s.ponomarev@gmail.com>
2023-01-10 15:17:17 +0100

    test

--- config.h
+++ config.h
@@ -166,7 +166,8 @@ static Key keys[] = {
 	TAGKEYS(			XK_8,		7)
 	TAGKEYS(			XK_9,		8)
 	/* { MODKEY,			XK_0,		view,		{.ui = ~0 } }, */
-	{ MODKEY|ShiftMask,		XK_0,				{.v = (const char*[]){ "betterlockscreen --lock blur", NULL } } },
+	{ MODKEY|ShiftMask,		XK_0,		spawn,		SHCMD("betterlockscreen --lock blur") },
+	/* { MODKEY|ShiftMask,		XK_0,				{.v = (const char*[]){ "betterlockscreen --lock blur", NULL } } }, */
 	{ MODKEY,			XK_F8,		spawn,		SHCMD("wpctl set-volume @DEFAULT_AUDIO_SINK@ 15%-; kill -44 $(pidof dwmblocks)") },
 	/* { MODKEY|ShiftMask,		XK_minus,	spawn,		SHCMD("wpctl set-volume @DEFAULT_AUDIO_SINK@ 15%-; kill -44 $(pidof dwmblocks)") }, */
 	{ MODKEY,			XK_F9,		spawn,		SHCMD("wpctl set-volume @DEFAULT_AUDIO_SINK@ 15%+; kill -44 $(pidof dwmblocks)") },
